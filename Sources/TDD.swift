075b2785d47726f9963f1efeef47e2e01102f4e7
Jeong-Uk Lee <julee@favorie.co>
2017-07-25 22:31:24 +0900

    9~13장
    통화 개념 도입, times 수정, Sum 구현

--- Sources/TDD.swift
+++ Sources/TDD.swift
@@ -1,38 +1,58 @@
 class Money {
     fileprivate let amount: Int
+    let currency: String
     
-    init(amount: Int) {
+    init(amount: Int, currency: String) {
         self.amount = amount
-    }
-    
-    class func dollar(amount: Int) -> Money {
-        return Dollar(amount: amount)
-    }
-    
-    class func franc(amount: Int) -> Money {
-        return Franc(amount: amount)
+        self.currency = currency
     }
     
     func times(multiplier: Int) -> Money {
-        return Money(amount: 0)
+        return Money(amount: self.amount * multiplier, currency: self.currency)
+    }
+}
+
+extension Money {
+    class func dollar(amount: Int) -> Money {
+        return Money(amount: amount, currency: "USD")
+    }
+    
+    class func franc(amount: Int) -> Money {
+        return Money(amount: amount, currency: "CHF")
     }
 }
 
 extension Money: Equatable {
     public static func == (lhs: Money, rhs: Money) -> Bool {
-        return (lhs as Money).amount == (rhs as Money).amount && (type(of: lhs) == type(of: rhs))
+        return lhs.amount == rhs.amount && lhs.currency == rhs.currency
     }
 }
 
-class Dollar: Money {
-    override func times(multiplier: Int) -> Money {
-        return Dollar(amount: self.amount * multiplier)
+extension Money: Expression {
+    static func + (augend: Money, addend: Money) -> Expression {
+        return Sum(augend: augend, addend: addend)
+    }
+    
+    func reduce(to: String) -> Money {
+        return self
     }
 }
 
-class Franc: Money {
-    override func times(multiplier: Int) -> Money {
-        return Franc(amount: self.amount * multiplier)
+protocol Expression {
+    func reduce(to: String) -> Money
+}
+
+class Bank {
+    func reduce(source: Expression, to: String) -> Money {
+        return source.reduce(to: to)
     }
 }
 
+struct Sum: Expression {
+    let augend: Money
+    let addend: Money
+    
+    func reduce(to: String) -> Money {
+        return Money(amount: self.augend.amount + self.addend.amount, currency: to)
+    }
+}
--- Tests/TDDTests/TDDTests.swift
+++ Tests/TDDTests/TDDTests.swift
@@ -2,25 +2,53 @@ import XCTest
 @testable import TDD
 
 class TDDTests: XCTestCase {
+    func testEquality() {
+        XCTAssertTrue(Money.dollar(amount: 5) ==  Money.dollar(amount: 5))
+        XCTAssertFalse(Money.dollar(amount: 5) == Money.dollar(amount: 6))
+        XCTAssertFalse(Money.franc(amount: 5) == Money.dollar(amount: 5))
+    }
+}
+
+extension TDDTests {
     func testMultiplication() {
         let five: Money = Money.dollar(amount: 5)
         XCTAssertEqual(Money.dollar(amount: 10), five.times(multiplier: 2))
         XCTAssertEqual(Money.dollar(amount: 15), five.times(multiplier: 3))
     }
     
-    func testEquality() {
-        XCTAssertTrue(Money.dollar(amount: 5) ==  Money.dollar(amount: 5))
-        XCTAssertFalse(Money.dollar(amount: 5) == Money.dollar(amount: 6))
-        XCTAssertTrue(Money.franc(amount: 5) ==  Money.franc(amount: 5))
-        XCTAssertFalse(Money.franc(amount: 5) == Money.franc(amount: 6))
-        
-        XCTAssertFalse(Money.franc(amount: 5) == Money.dollar(amount: 5))
+    func testSimpleAddition() {
+        let five = Money.dollar(amount: 5)
+        let sum = five + five
+        let bank = Bank()
+        let reduced = bank.reduce(source: sum, to: "USD")
+        XCTAssertEqual(Money.dollar(amount: 10), reduced)
     }
     
-    func testFrancMultiplication() {
-        let five = Money.franc(amount: 5)
-        
-        XCTAssertEqual(Money.franc(amount: 10), five.times(multiplier: 2))
-        XCTAssertEqual(Money.franc(amount: 15), five.times(multiplier: 3))
+    func testPlusReturnsSum() {
+        let five = Money.dollar(amount: 5)
+        let result = five + five
+        let sum = result as? Sum
+        XCTAssertEqual(Optional(five), sum?.augend)
+        XCTAssertEqual(Optional(five), sum?.addend)
+    }
+    
+    func testReduceSum() {
+        let sum = Sum(augend: Money.dollar(amount: 3), addend: Money.dollar(amount: 4))
+        let bank = Bank()
+        let result = bank.reduce(source: sum, to: "USD")
+        XCTAssertEqual(Money.dollar(amount: 7), result)
+    }
+    
+    func testReduceMoney() {
+        let bank = Bank()
+        let result = bank.reduce(source: Money.dollar(amount: 1), to: "USD")
+        XCTAssertEqual(Money.dollar(amount: 1), result)
+    }
+}
+
+extension TDDTests {
+    func testCurrency() {
+        XCTAssertEqual("USD", Money.dollar(amount: 1).currency)
+        XCTAssertEqual("CHF", Money.franc(amount: 1).currency)
     }
 }
